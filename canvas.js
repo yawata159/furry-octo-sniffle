e055f74afab52bb33dbea5ade471c29b34f1f081
Vincent Liok <vliok@stuy.edu>
2017-02-13 20:17:58 -0500

    added clear button

--- canvas.js
+++ canvas.js
@@ -3,6 +3,9 @@ var ctx = canvas.getContext('2d');
 var colors = ["#1abc9c","#2ecc71","#16a085","#27ae60","#3498db","#2980b9","#9b59b6","#8e44ad","#f1c40f","#f39c12","#e67e22","#e74c3c","#2c3e50"];
 canvas.addEventListener("click", createRectangle);
 
+var button = document.getElementById("clear");
+button.addEventListener("click", clearCanvas);
+
 function createRectangle(e) {
     var canvX = e.offsetX;
     var canvY = e.offsetY;
@@ -16,4 +19,8 @@ function createRectangle(e) {
 
 function randColor() {
     return colors[Math.floor(Math.random()*colors.length)];
+}
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, 700, 500);
 }
